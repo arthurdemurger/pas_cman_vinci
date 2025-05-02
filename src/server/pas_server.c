5c161df70315577e45bfd7ebd1d7a7e70b53cd4a
Arthur Demurger <arthur.demurger@student.vinci.be>
2025-05-02 13:35:20 +0200

    refactor: outsourcing function

--- src/server/pas_server.c
+++ src/server/pas_server.c
@@ -1,31 +1,38 @@
 #include "./pas_server.h"
 
-int main (void) {
-  // Declare a GameState structure to hold the game state
-  struct GameState state;
+#define PERM 0666
 
-  // Create a shared memory segment for the GameState structure
-  int shm_id = sshmget(IPC_PRIVATE, sizeof(struct GameState), IPC_CREAT | 0666);
-
-  // Attach the shared memory segment to the process's address space
+// Function to initialize shared memory and reset the game state
+struct GameState* initialize_shared_memory() {
+  int shm_id = sshmget(IPC_PRIVATE, sizeof(struct GameState), IPC_CREAT | PERM);
   struct GameState *shm_ptr = (struct GameState *) sshmat(shm_id);
-
-  // Reset the game state in the shared memory
   reset_gamestate(shm_ptr);
+  return shm_ptr;
+}
 
-  // Create a semaphore set with one semaphore
-  int sem_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
+// Function to initialize a semaphore
+int initialize_semaphore() {
+  int sem_id = semget(IPC_PRIVATE, 1, IPC_CREAT | PERM);
   checkNeg(sem_id, "Semget error");
 
-  // Initialize the semaphore value
   union semun arg;
   arg.val = 0; // semaphore value 0 -> blocked
-
-  if (semctl(sem_id, 0, SETVAL, arg) == -1) { // Apply the value to the semaphore
+  if (semctl(sem_id, 0, SETVAL, arg) == -1) {
     perror("Error semctl in sem_create");
     exit(EXIT_FAILURE);
   }
+  return sem_id;
+}
 
+
+int main(void) {
+  // Initialize shared memory and reset the game state}
+  struct GameState *shm_ptr = initialize_shared_memory();
+
+  // Initialize semaphore
+  int sem_id = initialize_semaphore();
+
+  // Initialize pipe
   int pipe_to_broadcaster[2];
   spipe(pipe_to_broadcaster);
 
