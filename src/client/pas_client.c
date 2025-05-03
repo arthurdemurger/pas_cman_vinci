82686b8406cf4f3957a0d0d70cc2e4ba56cce49f
Arthur Demurger <arthur.demurger@student.vinci.be>
2025-05-03 12:24:56 +0200

    feat: add port in args

--- src/client/pas_client.c
+++ src/client/pas_client.c
@@ -1,23 +1,27 @@
 #include "pas_client.h"
 
-int main(void) {
+int main(int ac, char **av) {
+  if (ac != 2) {
+    fprintf(stderr, "Usage: %s <port>\n", av[0]);
+    exit(EXIT_FAILURE);
+  }
+  int server_port = atoi(av[1]);
 
-    // Printf
-    printf("Bienvenue dans le PAS CMAN\n");
-    printf("Pour participer entrez votre nom :\n");
+  // Printf
+  printf("Bienvenue dans le PAS CMAN\n");
+  printf("Pour participer entrez votre nom :\n");
 
-    // Buffer pour l'input clavier
-    char pseudo[20];
+  // Buffer pour l'input clavier
+  char pseudo[20];
 
-    // Lecture clavier
-    int ret = sread(0, pseudo, 20);
+  // Lecture clavier
+  int ret = sread(0, pseudo, 20);
 
-    // Connexion au serveur
-    int sockfd = init_socket_client(SERVER_IP, SERVER_PORT);
+  // Connexion au serveur
+  int sockfd = init_socket_client(server_port);
 
-    // Écriture du pseudo dans le socket
-    // swrite(sockfd, pseudo, 20);
-
-    // sleep(30);
-    return 0;
+  // Écriture du pseudo dans le socket
+  // swrite(sockfd, pseudo, 20);
+  // sleep(30);
+  return 0;
 }
